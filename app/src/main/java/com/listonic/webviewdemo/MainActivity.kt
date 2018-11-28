7adfb680f105358aa6b392c0fd4efbc6da58474e
mpowloka <mpowloka@listonic.com>
2018-11-28 13:10:25 +0100

    Use Vitalia website

--- app/src/main/java/com/listonic/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/listonic/webviewdemo/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.listonic.webviewdemo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +15,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupWebClient()
         webView.settings.javaScriptEnabled = true
-        webView.loadUrl("https://app.listonic.com/testwidget.html")
+
+        webView.loadUrl("https://vitalia.pl/index.php/mid/134/fid/1734/kalorie/diety/action/reload/")
     }
+
+    private fun setupWebClient() {
+
+        webView.webViewClient = object : WebViewClient() {
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest): Boolean {
+
+                val host = Uri.parse(request.url.toString())?.host
+
+                if (host?.contains("listonic") == true) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(request.url.toString()))
+                    startActivity(intent)
+                    return true
+                }
+
+                return false
+            }
+        }
+    }
+
 }
